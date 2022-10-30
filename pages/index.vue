0858f6f66ddde9ebad1c2517ccee9ff81332df7c
Jimmy <1064425721@qq.com>
2022-10-31 00:15:00 +0800

    feat(home): 增加`canvas`动画

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <div>首页</div>
+    <canvas></canvas>
   </div>
 </template>
-<script lang="ts" setup>
-
+<script setup lang="ts">
+import { drawTao } from "~/untils/canvas/tao";
+import { nextTick } from "vue";
+nextTick(() => {
+  const el = document.querySelector("canvas") as HTMLCanvasElement;
+  drawTao(el);
+});
 </script>
-<style lang="scss">
-</style>
+
+<style lang="scss"></style>
---
+++ untils/canvas/tao.ts
@@ -0,0 +1,97 @@
+/*
+ * @Author: Jimmy 1064425721@qq.com
+ * @Date: 2022-10-30 23:19:49
+ * @LastEditors: Jimmy 1064425721@qq.com
+ * @LastEditTime: 2022-10-31 00:01:40
+ * @FilePath: \blogs-nuxt3\untils\canvas\tao.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const VITE_BASEURL = import.meta.env.VITE_BASEURL;
+export const drawTao = (el: HTMLCanvasElement) => {
+  // 屏幕宽高
+  const ScreenWidth = document.documentElement.clientWidth;
+  const ScreenHight = document.documentElement.clientHeight;
+  // 重绘canvas大小
+  el.setAttribute("width", ScreenWidth.toString());
+  el.setAttribute("height", ScreenHight.toString());
+
+  const ctx = el.getContext("2d") as CanvasRenderingContext2D;
+  // ！！！这里要写上全路径
+  // const tao: string = VITE__URL + "/src/assets/tao1.png";
+  const tao: string = "/assets/tao1.png";
+  let image: HTMLImageElement = new Image();
+  image.src = tao;
+  /**
+   * @desc 构建一个方法供多次调用渲染
+   * @desc 需要x,y随机生成花瓣
+   * @desc 需要每秒下坠个随机距离(固定范围)
+   * @desc 需要随机生成大小(固定范围)
+   * @desc 花瓣需要一个每秒随机数翻转角度
+   */
+  function Tao(
+    this: any,
+    id: number,
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    rotate: number
+  ) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.ctx = ctx;
+    this.rotate = rotate;
+  }
+
+  // 绘画
+  Tao.prototype.draw = function () {
+    this.ctx.drawImage(image, this.x, this.y, 15, 15);
+  };
+
+  let coordinates = 0;
+  // 移动
+  Tao.prototype.move = function () {
+    this.y += 1.0;
+    coordinates += 1;
+    if (coordinates < 3) {
+      this.x += -0.8;
+    } else {
+      this.x += 0.8;
+      if (coordinates > 4) {
+        coordinates = 0;
+      }
+    }
+    if (this.y > ScreenWidth) {
+      this.y = 0;
+      this.x = Math.floor(Math.random() * ScreenWidth) + 100;
+    }
+    this.draw();
+  };
+
+  let taoArr: any = [];
+  // 初始化
+  const init = (ctx: CanvasRenderingContext2D) => {
+    for (let i = 0; i < 120; i++) {
+      taoArr[i] = new (Tao as any)(
+        i,
+        ctx,
+        Math.floor(Math.random() * ScreenWidth),
+        Math.floor(Math.random() * ScreenWidth),
+        (30 * Math.PI) / 180
+      );
+      taoArr[i].draw();
+    }
+  };
+
+  const animate = () => {
+    // 不断清除画布内容再绘制,
+    ctx.clearRect(0, 0, ScreenWidth, ScreenHight);
+    for (var i in taoArr) {
+      taoArr[i].move();
+    }
+    // 按帧对网页进行重绘
+    requestAnimationFrame(animate);
+  };
+  init(ctx);
+  animate();
+};
